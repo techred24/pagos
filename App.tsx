f31f5a6b794fca21942696d5c82fcd1503a940e1
TeChRF24 <f.rafael.temich24@gmail.com>
2025-09-08 13:20:16 -0600

    Primer commit

--- App.tsx
+++ App.tsx
@@ -5,23 +5,121 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+// import { useState } from 'react';
+// import { StyleSheet, View, Text, Pressable, Platform, Button } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
+type RootStackParamList = {
+  Home: undefined;
+  Details: undefined;
+  AgregarCliente: undefined;
+};
+
+type HomeScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, 'Home'>;
+
+function HomeScreen({ navigation }: { navigation: HomeScreenNavigationProp }) {
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Pressable
+            style={({ pressed }) => [
+                styles.button,
+                {
+                    backgroundColor: pressed ? '#5856D6' : 'skyblue',
+                },
+            ]}
+            onPress={() => {navigation.navigate('AgregarCliente');}}
+            onLongPress={() => {}}>
+            {({ pressed }) => (
+              <Text style={{ color: pressed ? '#FFF' : '#000' }}>
+                Agregar Cliente
+              </Text>
+            )}
+      </Pressable>
+      <Pressable
+        style={( pressed ) => [
+            styles.button,
+            {
+                backgroundColor: pressed ? '#5856D6' : 'skyblue',
+            },
+        ]}
+        onPress={() => navigation.navigate('Details')}
+      >
+        {({ pressed }) => (
+          <Text style={{ color: pressed ? '#FFF' : "#000"}}>
+            Agregar Prestamo
+          </Text>
+        )}
+      </Pressable>
     </View>
   );
 }
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Pantalla de Detalles</Text>
+    </View>
+  );
+}
+function AgregarClienteScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Pantalla para agregar clientes</Text>
+    </View>
+  )
+}
+const Stack = createNativeStackNavigator();
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="AgregarCliente" component={AgregarClienteScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+// function App() {
+//   const [ number, setNumber ] = useState<number>(0);
+//   return (
+//     <View style={styles.container}>
+//       <Text>{number}</Text>
+//       <Pressable 
+//             style={({ pressed }) => [
+//                 styles.button,
+//                 {
+//                     backgroundColor: pressed ? '#5856D6' : 'skyblue',
+//                 },
+//             ]}
+//             onPress={() => {setNumber((current: number) => ++current);}}
+//             onLongPress={() => {setNumber(0);}}>
+//         {/* <Text>Incrementar</Text> */}
+//             {({ pressed }) => (
+//               <Text style={{ color: pressed ? '#FFF' : '#000' }}>
+//                 Incrementar
+//               </Text>
+//             )}
+//       </Pressable>
+//     </View>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#FFF',
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    // backgroundColor: Platform.OS === 'ios' ? '#5856D6' : '#000',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 10,
   },
 });
 
